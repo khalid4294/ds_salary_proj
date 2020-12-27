7d31a0a7ee6f6766e5ad27efbc590af5de575ecd
khalid <alolayankhalid@gmail.com>
2020-12-27 11:53:00 +0300

    data cleaned

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import scraping as gs
 import pandas as pd
-path = "/Users/khalidalolayan/Desktop/document/ds_salary_proj/chromedriver"
+path = "/Users/khalidalolayan/Desktop/ds_salary_project/chromedriver"
 df = gs.get_jobs('marketing manager', 15, False, path, 15)
